82a1d4c7143217466bc87ebb796ce40a0465e685
ari <61393419+eftpmc@users.noreply.github.com>
2026-01-14 13:30:46 -0500

    repeatmode

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/models/QueuedPlayerOptions.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/models/QueuedPlayerOptions.kt
@@ -12,3 +12,18 @@ class DefaultQueuedPlayerOptions(
     override var alwaysPauseOnInterruption: Boolean = false,
     override var crossfadeDurationMs: Long = 0L,
 ) : QueuedPlayerOptions
+
+enum class RepeatMode {
+    OFF, ONE, ALL;
+
+    companion object {
+        fun fromOrdinal(ordinal: Int): RepeatMode {
+            return when (ordinal) {
+                0 -> OFF
+                1 -> ONE
+                2 -> ALL
+                else -> OFF
+            }
+        }
+    }
+}
