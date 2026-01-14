c0f5e1fd050206ba8c9c308ab180c2300667f7ad
ari <61393419+eftpmc@users.noreply.github.com>
2026-01-14 13:43:01 -0500

    repeatmode

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/models/QueuedPlayerOptions.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/models/QueuedPlayerOptions.kt
@@ -5,11 +5,13 @@ import com.google.android.exoplayer2.Player
 
 interface QueuedPlayerOptions : PlayerOptions {
     override var alwaysPauseOnInterruption: Boolean
+    var repeatMode: RepeatMode
     var crossfadeDurationMs: Long
 }
 
 class DefaultQueuedPlayerOptions(
     override var alwaysPauseOnInterruption: Boolean = false,
+    override var repeatMode: RepeatMode = RepeatMode.OFF,
     override var crossfadeDurationMs: Long = 0L,
 ) : QueuedPlayerOptions
 
